5f226ac41e6d3f48dc2d396f2780e9677b594096
yauheni baltsevich <norcobeatz@gmail.com>
2023-01-22 17:12:42 +0100

    docs: add all sections for adaptive 380px

--- plants/script.js
+++ plants/script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("header-burger").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
+});
+
 console.log("Самостоятельная оценка: 100 баллов\n1. Верстка валидная +10\n2. Верстка семантическая +20\n3. Верстка соответствует макету +48\n4. Требования к css +12\n5. Интерактивность +20");
