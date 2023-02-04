1ce3646f7d46403df6cc3a53071c264d39d35734
eugenebalts <norcobeatz@gmail.coml@gmail.com>
2023-02-04 19:52:58 +0100

    docs: already finished accordeon

--- plants/script.js
+++ plants/script.js
@@ -19,3 +19,45 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+
+// ACCORDION
+
+
+const pricesMenu = document.querySelectorAll(".prices-selection-item");
+
+console.log(pricesMenu)
+
+pricesMenu.forEach(function(item) {
+    item.addEventListener('click', function (e) {
+        item.classList.remove('show')
+
+        pricesMenu.forEach(function(item) {
+            item.classList.remove('show')
+            e.target.closest('.prices-selection-item').classList.add('show')
+        })
+    })
+})
+
+function hasClass(element, className) {
+    var rx = new RegExp('(?:^| )' + className + '(?: |$)');
+    return rx.test(element.className);
+}
+
+pricesMenu.addEventListener('click', function)
+
+// pricesMenu.forEach(function(item) => {
+//     if (item)
+// })
+
+
+// const serviceButton = document.getElementById('service-button-gardeners')
+
+// function handleClick (event) {
+//     this.
+//     }
+
+
+// serviceButton.addEventListener('click', handleClick)
+
+
+
