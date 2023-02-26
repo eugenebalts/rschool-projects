06a7ecbebd1e0183e73219df1365eb655617284b
eugenebalts <norcobeatz@gmail.coml@gmail.com>
2023-02-26 16:06:43 +0100

    docs: create audio and playlist

---
+++ momentum/playlist.js
@@ -0,0 +1,2 @@
+const songs = ['Aqua Caelestis', 'Ennio Morricone', 'River Flows In You', 'Summer Wind'];
+export default songs;
--- momentum/script.js
+++ momentum/script.js
@@ -1,30 +1,4 @@
 // TIME & DATE
-const time = document.querySelector('.time');
-const date = document.querySelector('.date');
-let actualDate = new Date();
-let currentTime = actualDate.toLocaleTimeString(); // ONLY TIME
-// ONLY DATE
-const options = {date: 'long',month: 'long', day: 'numeric'};
-let currentDate = actualDate.toLocaleDateString('en-US', options);
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let timeOfDay;
-
-const greeting = document.querySelector('.greeting');
-
-const name = document.querySelector('.name');
-const body = document.querySelector('body');
-
-let randomNum;
-let bgNum;
-
-const slideNext = document.querySelector('.slide-next');
-const slidePrev = document.querySelector('.slide-prev');
-
-const weatherIcon = document.querySelector('.weather-icon');
-const temperature = document.querySelector('.temperature');
-const weatherDescription = document.querySelector('.weather-description');
-
-const city = document.querySelector('.city');
 
 const citation = [
     {
@@ -54,13 +28,70 @@ const citation = [
     {
         'text': "Only a life lived for others is a life worthwhile.",
         'author': 'Albert Einstein',
-    }
+    },
 ];
+
+const songs = ['Aqua Caelestis', 'Ennio Morricone', 'River Flows In You', 'Summer Wind'];
+
+
+// DATE
+
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
+let actualDate = new Date();
+let currentTime = actualDate.toLocaleTimeString(); // ONLY TIME
+const options = {date: 'long',month: 'long', day: 'numeric'};
+let currentDate = actualDate.toLocaleDateString('en-US', options);
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let timeOfDay;
+
+// GREETING
+
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
+const body = document.querySelector('body');
+
+let randomNum;
+let bgNum;
+
+// BACKGROUND
+
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+
+// WEATHER 
+
+const weatherIcon = document.querySelector('.weather-icon'),
+    temperature = document.querySelector('.temperature'),
+    weatherDescription = document.querySelector('.weather-description'),
+    wind = document.querySelector('.wind'),
+    humidity = document.querySelector('.humidity')
+
+const city = document.querySelector('.city');
+
+//QUOTES
 
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const changeQuote = document.querySelector('.change-quote');
 
+// AUDIO
+
+const player = document.querySelector('.player'),
+    playerPrev = document.querySelector('.play-prev'),
+    playerPlay = document.querySelector('.play'),
+    playerNext = document.querySelector('.play-next'),
+    songName = document.querySelector('.song-name'),
+    audio = document.querySelector('.audio'),
+    playList = document.querySelector('.play-list');
+    progressContainer = document.querySelector('.progress-container')
+    progress = document.querySelector('.progress'),
+    audioCurrentTime = document.querySelector('.audio-current-time');
+    audioLength = document.querySelector('.audio-length')
+
+
+let songIndex = 0;
+const duration = audio.duration;
 
 
 
@@ -90,14 +121,14 @@ showTime();
 
 function getTimeOfDay() {
     let currentHour = actualDate.getHours();
-    if (currentHour >= 00 && currentHour < 12) {
+    if (currentHour >= 6 && currentHour < 12) {
         timeOfDay = 'Morning';
-    } else if (currentHour >= 12 && currentHour < 17) {
+    } else if (currentHour >= 12 && currentHour < 18) {
         timeOfDay = 'Afternoon'
-    } else if (currentHour >= 17 && currentHour < 20) {
+    } else if (currentHour >= 18 && currentHour < 24) {
         timeOfDay = 'Evening'
     } else timeOfDay = 'Night'
-    greeting.textContent = `Good ${timeOfDay}`
+    greeting.textContent = `Good ${timeOfDay}, `
 }
 getTimeOfDay();
 
@@ -105,6 +136,7 @@ getTimeOfDay();
 
 //REMEMBER NAME FROM INPUT
 
+name.placeholder = "[Enter name]";
 function setLocalStorage() {
     localStorage.setItem('name', name.value);
 }
@@ -187,6 +219,9 @@ async function getWeather() {
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
     temperature.textContent = `${Math.round(data.main.temp)}°C`;
     weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`
+
 }
 
 function setCity(event) {
@@ -235,18 +270,186 @@ setQuote();
 changeQuote.addEventListener('click', refreshQuote);
 
 function refreshQuote() {
+    // let actualQuoteNumber = randomQuoteNumber;
     let actualQuoteNumber = randomQuoteNumber;
-    randomQuoteNumber = Math.round(Math.random() * (citation.length - 1));
-    if (actualQuoteNumber === randomQuoteNumber) {
-        randomQuoteNumber = randomQuoteNumber + 1;
-        
+    newRandomQuote = Math.round(Math.random() * (citation.length - 1));
+    // randomQuoteNumber = Math.round(Math.random() * (citation.length - 1));
+    if (actualQuoteNumber !== newRandomQuote) {
+        randomQuoteNumber = newRandomQuote;
+    } else {
+        randomQuoteNumber = newRandomQuote + 1;
     }
-    randomQuoteNumber >= citation.length ? randomQuoteNumber = randomQuoteNumber - 1 : randomQuoteNumber < 0 ? randomQuoteNumber = randomQuoteNumber + 1 : {};
 
-    console.log(randomQuoteNumber)
+    if (randomQuoteNumber === citation.length) {
+        randomQuoteNumber = 0;
+    }
+
+    // randomQuoteNumber >= citation.length ? randomQuoteNumber = randomQuoteNumber - 1 : randomQuoteNumber < 0 ? randomQuoteNumber = randomQuoteNumber + 1 : {};
+
+    // console.log(randomQuoteNumber)
     setQuote()
-
 }
+
+
+
+// AUDIO
+
+
+audio.volume = 0.2;
+
+function loadSong(song) {
+    audio.src = `assets/sounds/${song}.mp3`
+}
+loadSong(songs[songIndex]);
+
+songName.textContent = songs[songIndex];
+
+function playAudio() {
+    player.classList.add('playing');
+    audio.play();
+    playerPlay.style.backgroundImage = ' url("assets/svg/pause.svg")';
+}
+
+function pauseAudio() {
+    player.classList.remove('playing')
+    audio.pause();
+    playerPlay.style.backgroundImage = ' url("assets/svg/play.svg")';
+}
+
+playerPlay.addEventListener('click', () => {
+    const isPlaying = player.classList.contains('playing');
+    if (isPlaying) {
+        pauseAudio()
+    } else {
+        playAudio()
+    }
+})
+
+function nextAudio() {
+    songIndex += + 1;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    songName.textContent = songs[songIndex];
+
+    playAudio();
+}
+
+function prevAudio() {
+    songIndex -= 1;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    songName.textContent = songs[songIndex];
+    playAudio();
+}
+
+playerNext.addEventListener('click', nextAudio);
+playerPrev.addEventListener('click', prevAudio);
+
+// SONG PREVIEW
+
+
+
+// FILL PLAYLIST
+    songs.forEach(item => {
+        let li = document.createElement('li');
+        li.textContent = item;
+        li.classList.add('play-item');
+        playList.append(li);
+    })
+
+    const songInPlaylist = document.querySelectorAll('.play-item');
+
+// // // // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+        songInPlaylist.forEach((song, index) => {
+            song.addEventListener('click', (e) => {
+                songName.textContent = e.target.textContent;
+                songInPlaylist.forEach(item => {
+                    item.classList.remove('playing');
+                })
+                e.target.classList.add('playing')
+                loadSong(songs[index])
+                playAudio()
+            })
+    })
+
+
+
+// Progress bar
+function progressBar(event) {
+
+
+    const {duration, currentTime} = event.srcElement;
+    const progressionPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressionPercent}%`;
+    // console.log(audio.currentTime)
+    audioCurrentTime.textContent = getTimeCodeFromNum(audio.currentTime);
+
+    if (audio.currentTime === audio.duration) {
+        return nextAudio()
+    }
+}
+
+function rewindAudio(event) {
+    const width = this.clientWidth;
+    const push = event.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (push / width) * duration;
+}
+
+function getTimeCodeFromNum(num) {
+    let seconds = parseInt(num);
+    let minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+    const hours = parseInt(minutes / 60);
+    minutes -= hours * 60;
+
+    if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+    return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+    ).padStart(2, 0)}`;
+}
+
+
+
+audio.addEventListener('timeupdate', progressBar)
+progressContainer.addEventListener('click', rewindAudio);
+
+audio.addEventListener("loadeddata", () => {
+    audioLength.textContent = getTimeCodeFromNum(
+        audio.duration
+    );
+    ;
+
+    audio.volume = .1;
+    },  
+    false
+);
+
+// song.style.setProperty('--itemBeforeBackImg','url("../assets/svg/play.svg")');
+
+
+
+// API BACKGROUND
+
+// function getLinkToImage() {
+//     const url = fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=zD3nIyvyKbW6cB3dmNv-j8iXoIbf9_Nhum4eL4L-_UQ')
+//     console.log(url)
+// }
+// getLinkToImage()
+
+
+
+
+
+
+
 
 
 
